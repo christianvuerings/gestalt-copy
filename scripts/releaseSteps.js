edc8f8ab991542bfeadaa004caeeecfe7a5e1b8e
Christian Vuerings <cvuerings@pinterest.com>
2020-05-11 08:22:01 -0700

    Fix 11: Update bot + add console messages #minor

--- scripts/releaseSteps.js
+++ scripts/releaseSteps.js
@@ -62,10 +62,7 @@ async function bumpPackageVersion({ lastCommitMessage }) {
     `${JSON.stringify(packageJSONParsed, null, 2)}\n`
   );
 
-  console.log(`Previous version: ${previousVersion}`);
-  console.log(`New version: ${newVersion}`);
-
-  return { newVersion, releaseType };
+  return { previousVersion, newVersion, releaseType };
 }
 
 async function updateChangelog({ releaseNotes }) {
@@ -84,7 +81,7 @@ ${previousChangelog}`
 
 async function commitChanges({ newVersion }) {
   shell.exec('git add .');
-  shell.exec('git config --global user.email "gestalt-bot@pinterest.com"');
+  shell.exec('git config --global user.email "pinterest.gestalt@gmail.com"');
   shell.exec('git config --global user.name "Gestalt Bot"');
   shell.exec(`git commit -am "Version bump: v${newVersion}"`);
   shell.exec('git push --set-upstream origin master');
@@ -106,27 +103,50 @@ async function createGitHubRelease({ newVersion, releaseNotes }) {
     data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl },
   } = createReleaseResponse;
 
-  console.log('id', releaseId);
-  console.log('html_url', htmlUrl);
-  console.log('upload_url', uploadUrl);
+  return { releaseId, htmlUrl, uploadUrl };
 }
 
 (async () => {
   console.log('Running Gestalt Release Steps');
 
+  console.log('\nGet last commit message');
   const lastCommitMessage = await getLastCommitMessage();
-  const { newVersion, releaseType } = await bumpPackageVersion({
-    lastCommitMessage,
-  });
+  console.log(`Last commit message: ${lastCommitMessage}`);
+
+  console.log('\nGet last commit message');
+  const { newVersion, previousVersion, releaseType } = await bumpPackageVersion(
+    {
+      lastCommitMessage,
+    }
+  );
+  console.log(`Release type: ${releaseType}`);
+  console.log(`Previous version: ${previousVersion}`);
+  console.log(`New version: ${newVersion}`);
+
+  console.log(`\nRelease notes`);
   const releaseNotes = await getReleaseNotes({
     lastCommitMessage,
     newVersion,
     releaseType,
   });
+  console.log(`Release notes: ${releaseNotes}`);
+
+  console.log('\nUpdate Changelog');
   await updateChangelog({ releaseNotes });
+
+  console.log('\nCommit Changes');
   await commitChanges({ newVersion });
-  await createGitHubRelease({ newVersion, releaseNotes });
+
+  console.log('\nCreate GitHub Release');
+  const { releaseId, htmlUrl, uploadUrl } = await createGitHubRelease({
+    newVersion,
+    releaseNotes,
+  });
+  console.log('id', releaseId);
+  console.log('html_url', htmlUrl);
+  console.log('upload_url', uploadUrl);
 
   // Export new version so it can be used by other steps
+  console.log('\nOutput new version');
   core.setOutput('VERSION', newVersion);
 })();
